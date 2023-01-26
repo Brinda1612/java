e7a9e541975e707f9778a6f36255d284b4f0e48c
Brinda161 <brindaponkiya1000@gmail.com>
2023-01-26 20:19:46 +0530

    changes

---
+++ marks.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class marks {
+    public static void main(String[] args) {
+        
+     
+        Scanner M = new Scanner(System.in);
+        System.out.print("enter mark maths :");
+        int a = M.nextInt();
+        System.out.print("enter mark phy :");
+        int b = M.nextInt();
+        System.out.print("enter mark bio :");
+        int c = M.nextInt();
+        System.out.print("enter mark eng :");
+        int d = M.nextInt();
+        int sum = a+b+c+d;
+        int per = (sum*100/400);
+        System.out.println("total marks of :" + per);
+    }
+}
